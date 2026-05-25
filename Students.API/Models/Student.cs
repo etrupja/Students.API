408298e043534d7281c6c19fe5592d0594a840a0
Ervis Trupja <ervistrupja@outlook.com>
2026-05-25 23:16:27 +0200

    Update Student.cs

--- Students.API/Models/Student.cs
+++ Students.API/Models/Student.cs
@@ -4,11 +4,6 @@ public class Student
 {
     public int Id { get; set; }
     public string Name { get; set; } = string.Empty;
-    public Grades Grades { get; set; } = new Grades();
-}
-
-public class Grades
-{
     public double Math { get; set; }
     public double English { get; set; }
     public double Science { get; set; }
